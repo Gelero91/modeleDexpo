8291815f3c8b920800b3aec2bd1d78b5034d7a01
Catoire-dev <cy.catoire.dev@gmail.com>
2024-01-18 17:20:33 +0100

    wip: En attente du btn component, layout module et base style module

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
-import styles from './footer.module.css'; // Importez le fichier CSS pour ce composant
-
+import styles from './footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      {/* <Link href="/rgpd">
-        <button className={styles.button}>RGPD</button>
-      </Link>
-      <br/>
-      <Link href="/cgu">
-        <button className={styles.button}>CGU</button>
-      </Link> */}
+
       <Button path="/rgpd" text="RGPD"/>
       <Button path="/cgu" text="CGU"/>
       2023, Konexio
