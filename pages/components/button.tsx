b87c8cf21da60887d54bf06a01ac05b74a3d7f65
Catoire-dev <cy.catoire.dev@gmail.com>
2024-01-18 17:12:56 +0100

    feat : add classname to btn props, add css for btn

--- pages/components/button.tsx
+++ pages/components/button.tsx
@@ -1,14 +1,16 @@
 import Link from "next/link";
+import btnStyle from './button.module.css';
 
 type buttonProps = {
     text: string;
     path: string;
+    className: string;
 }
 
-export const Button: React.FC<buttonProps> = ({text, path}) => {
+export const Button: React.FC<buttonProps> = ({text, path, className}) => {
     return (
         <Link href={path}>
-            <button>{text}</button>
+            <button className={`${className} ${btnStyle.btn}`}>{text}</button>
         </Link>
     );
 }
