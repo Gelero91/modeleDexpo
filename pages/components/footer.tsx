9283e9f82d548374ee4a39c0d3ddd246f10c0e1a
Catoire-dev <cy.catoire.dev@gmail.com>
2024-01-18 16:34:49 +0100

    feat: improve footer

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -5,14 +5,15 @@ import styles from './footer.module.css'; // Importez le fichier CSS pour ce com
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Link href="/rgpd">
+      {/* <Link href="/rgpd">
         <button className={styles.button}>RGPD</button>
       </Link>
       <br/>
       <Link href="/cgu">
         <button className={styles.button}>CGU</button>
-      </Link>
-      <br/>
+      </Link> */}
+      <Button path="/rgpd" text="RGPD"/>
+      <Button path="/cgu" text="CGU"/>
       2023, Konexio
     </footer>
   );
